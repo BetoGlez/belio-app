943d5df1e7b9fcef9dac7bf37a6154e239bc7ccb
Alberto González <agonzalez@tribalyte.com>
2021-01-13 23:15:20 +0100

    Initial Commit

---
+++ belio-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  belio-app
+//
+//  Created by Alberto González Hernández on 13/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
